1a3096e37c639377ea639d388de5b0f47556bb53
Naveenkumar <naveensg.23@gmail.com>
2022-12-01 14:45:03 +0530

    1 12 22

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+
+import  {BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+function App() {
+  return (
+      <BrowserRouter>
+      <div>
+      <header>
+        <Link to={"/"}>Amazona</Link>
+      </header>
+      <main>
+      <Routes>
+        <Route path="/" element={<HomeScreen/>}/>
+        <Route path="/product/:slug" element={<ProductScreen/>}/>
+      </Routes>
+      </main>
+      </div>
+      </BrowserRouter>
+    
+    
+  );
+}
+
+export default App;
